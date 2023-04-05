a499548798a4dd9f6ba70d86212943d17e763833
Kalixt044 <igallardo2793@gmail.com>
2023-04-05 03:25:26 -0500

    Hola mundo, agregué un archivo requeriments.txt

--- src/challenges/js/30DaysJs-05/src/exercise.js
+++ src/challenges/js/30DaysJs-05/src/exercise.js
@@ -1,3 +1,26 @@
 export function findFamousCats(cats) {
-  // Tu código aquí 👈
+// Encontrar el máximo número de seguidores entre todos los gatos
+  let maxFollowers = 0;
+  for (let i = 0; i < cats.length; i++) {
+    const followers = cats[i].followers;
+    for (let j = 0; j < followers.length; j++) {
+      if (followers[j] > maxFollowers) {
+        maxFollowers = followers[j];
+      }
+    }
+  }
+  
+  // Encontrar los gatos con el máximo número de seguidores
+  const famousCats = [];
+  for (let i = 0; i < cats.length; i++) {
+    const followers = cats[i].followers;
+    for (let j = 0; j < followers.length; j++) {
+      if (followers[j] === maxFollowers) {
+        famousCats.push(cats[i].name);
+        break;
+      }
+    }
+  }
+  
+  return famousCats;
 }
