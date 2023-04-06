13378717b5ebe51d5c6a6725d67a830e1c8cefa8
Kalixt044 <igallardo2793@gmail.com>
2023-04-06 10:27:49 -0500

    Commit requeriments.txt

--- src/challenges/js/30DaysJs-11/src/exercise.js
+++ src/challenges/js/30DaysJs-11/src/exercise.js
@@ -1,3 +1,12 @@
 export function sendEmail(email, subject, body) {
-  // Tu código aquí 👈
+return new Promise((resolve, reject) => {
+    if (!email || !subject || !body) {
+      reject(new Error("Hacen falta campos para enviar el email"));
+    } else {
+      window.setTimeout(() => {
+        resolve({ email, subject, body });
+      }, 2000);
+    }
+  });
+
 }
