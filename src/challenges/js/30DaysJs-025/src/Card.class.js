52f7179e9d9b42f5d80a3c827a2e173d5eae1b63
root <root@Deco.localdomain>
2023-04-20 14:42:12 -0500

    Hola mundo

--- src/challenges/js/30DaysJs-025/src/Card.class.js
+++ src/challenges/js/30DaysJs-025/src/Card.class.js
@@ -1,5 +1,16 @@
 import { Pay } from "./Pay.class.js";
 
 export class Card extends Pay {
-  // Tu código aquí 👈
-}
+  constructor(number) {
+    super();
+    this.number = number;
+  }
+
+  makePay(quantity) {
+    if (this.number.length !== 16) {
+      return {
+        realized: false,
+        quantity: 0,
+        error: "Invalid card number"
+      }
+    }
