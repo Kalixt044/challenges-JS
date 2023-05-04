e9062f7f0d2f600271e9fd81d9f588ca5982a2ef
Isidro J. Gallardo Navarro <igallardo2793@gmail.com>
2023-05-03 20:28:21 -0500

    Respuesta incorrecta

--- src/challenges/js/fibonacci/src/exercise.js
+++ src/challenges/js/fibonacci/src/exercise.js
@@ -1,3 +1,15 @@
 export function fibonacci(number) {
-  // Tu código aquí 👈
+  const fibo = [];
+  let a = 1;
+  let b = 0;
+  let temp;
+
+  for (let i = 0; i < n; i++) {
+    fibo.push(a);
+    temp = a;
+    a = a + b;
+    b = temp;
+  }
+
+  return fibo;
 }
