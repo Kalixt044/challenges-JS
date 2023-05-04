a496f2185d36c5839fbd62c8d0d35a0b28dfedb7
Isidro J. Gallardo Navarro <igallardo2793@gmail.com>
2023-05-03 20:09:23 -0500

    Ejercicio cuenta palabras

--- src/challenges/js/count-words/src/exercise.js
+++ src/challenges/js/count-words/src/exercise.js
@@ -1,3 +1,8 @@
 export function countWords(array) {
-  // Tu código aquí 👈
+  let total_words = 0;
+  for (let i = 0; i < array.length; i++) {
+    let words = array[i].split(' ');
+    total_words += words.length;
+  }
+  return total_words;
 }
