9d32e095b583a25525a7d5495b0be7beb3dca77d
Isidro J. Gallardo Navarro <igallardo2793@gmail.com>
2023-05-04 16:05:02 -0500

    Resuelto

--- src/challenges/js/check-prime/src/exercise.js
+++ src/challenges/js/check-prime/src/exercise.js
@@ -1,3 +1,12 @@
 export function trialDivision(number) {
-  // Tu código aquí 👈
+  if (number % 1 !== 0 || number <= 1) {
+    return false;
+  }
+  
+  for (let i = 2; i <= Math.sqrt(number); i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
