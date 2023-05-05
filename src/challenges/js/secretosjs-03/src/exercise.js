f6ec31ee92217d67325a6413ae5ec7b9b521300c
Isidro J. Gallardo Navarro <igallardo2793@gmail.com>
2023-05-04 23:04:16 -0500

    Exercise

--- src/challenges/js/secretosjs-03/src/exercise.js
+++ src/challenges/js/secretosjs-03/src/exercise.js
@@ -1,24 +1,70 @@
 export class Motor {
   constructor(propulsionTo, historyInstance) {
     let status = "off"
+    let lastChange = null;
     let history = historyInstance;
 
     this.getStatus = () => status;
+    
     let setStatus = (newStatus) => {
-      status = newStatus;
+      if (status !== newStatus) {
+        let newChange = { propulsionTo, status: newStatus };
+        if (!lastChange || JSON.stringify(newChange) !== JSON.stringify(lastChange)) {
+          history.addChange(newChange);
+          lastChange = newChange;
+        }
+        status = newStatus;
+      }
     };
 
     this.turnOn = () => setStatus("on");
     this.turnOff = () => setStatus("off");
     
     this.getHistory = () => history.getFullState();
+  }export class Motor {
+    constructor(propulsionTo, historyInstance) {
+      let status = "off"
+      let lastChange = null;
+      let history = historyInstance;
+  
+      this.getStatus = () => status;
+      
+      let setStatus = (newStatus) => {
+        if (status !== newStatus) {
+          let newChange = { propulsionTo, status: newStatus };
+          if (!lastChange || JSON.stringify(newChange) !== JSON.stringify(lastChange)) {
+            history.addChange(newChange);
+            lastChange = newChange;
+          }
+          status = newStatus;
+        }
+      };
+  
+      this.turnOn = () => setStatus("on");
+      this.turnOff = () => setStatus("off");
+      
+      this.getHistory = () => history.getFullState();
+    }
   }
-}
+  
+  export class History {
+    constructor() {
+      let changes = [];
+  
+      this.addChange = (change) => changes.push(change);
+  
+      this.getFullState = () => changes;
+    }
+  }
+  
 
 export class History {
   constructor() {
-    let history = [];
+    let changes = [];
 
-    this.getFullState = () => history;
+    this.addChange = (change) => changes.push(change);
+
+    this.getFullState = () => changes;
   }
 }
+
