5ce1c47c3a2f8962279bc57555b6c86ce39bd882
Isidro J. Gallardo Navarro <igallardo2793@gmail.com>
2023-05-05 09:03:46 -0500

    esta no es otra cancion de amor

--- src/challenges/js/return-tasks/src/exercise.js
+++ src/challenges/js/return-tasks/src/exercise.js
@@ -1,5 +1,13 @@
 import { doTask1, doTask2, doTask3 } from './tasks';
 
-export function runCode() {
-  // Tu código aquí 👈
+export async function runCode() {
+  try {
+    const result1 = await doTask1();
+    const result2 = await doTask2();
+    const result3 = await doTask3();
+
+    return [result1, result2, result3];
+  } catch (error) {
+    console.error(error);
+  }
 }
