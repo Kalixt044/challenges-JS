568365b07d6308a9bb403f8ef64be0a2e6bb422f
Isidro J. Gallardo Navarro <igallardo2793@gmail.com>
2023-05-09 21:59:22 -0500

    solution +  requeriments

--- src/challenges/js/sort-by-lastname/src/exercise.js
+++ src/challenges/js/sort-by-lastname/src/exercise.js
@@ -1,3 +1,26 @@
 export function sortByLastName(array) {
-  // Tu código aquí 👈
+  // Utilizamos el método sort() para ordenar el array
+  array.sort((a, b) => {
+    // Separamos los nombres y apellidos por espacio en cada elemento
+    const [apellidoA, nombreA] = a.split(' ');
+    const [apellidoB, nombreB] = b.split(' ');
+
+    // Comparamos los apellidos para determinar el orden
+    if (apellidoA < apellidoB) {
+      return -1; // a debe ir antes que b
+    } else if (apellidoA > apellidoB) {
+      return 1; // a debe ir después que b
+    } else {
+      // Si los apellidos son iguales, comparamos los nombres
+      if (nombreA < nombreB) {
+        return -1;
+      } else if (nombreA > nombreB) {
+        return 1;
+      } else {
+        return 0; // Son iguales
+      }
+    }
+  });
+
+  return array;
 }
