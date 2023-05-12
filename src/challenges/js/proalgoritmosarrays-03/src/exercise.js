9b7a79e03e41c282badeb7276373daf1bd1aa390
Isidro J. Gallardo Navarro <igallardo2793@gmail.com>
2023-05-11 22:25:43 -0500

    code

--- src/challenges/js/proalgoritmosarrays-03/src/exercise.js
+++ src/challenges/js/proalgoritmosarrays-03/src/exercise.js
@@ -1,3 +1,23 @@
 export function maxArea(alturas) {
-  // Tu código aquí 👈
+  let maxArea = 0;
+  let left = 0;
+  let right = alturas.length - 1;
+
+  while (left < right) {
+    const width = right - left;
+    const height = Math.min(alturas[left], alturas[right]);
+    const area = width * height;
+
+    if (area > maxArea) {
+      maxArea = area;
+    }
+
+    if (alturas[left] < alturas[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxArea;
 }
