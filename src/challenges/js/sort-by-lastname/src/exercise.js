d466b817a9835e125c1d87aec30d6a59dea6d97d
Isidro J. Gallardo Navarro <igallardo2793@gmail.com>
2023-07-19 15:45:59 -0500

    empezamos desde cero

--- src/challenges/js/sort-by-lastname/src/exercise.js
+++ src/challenges/js/sort-by-lastname/src/exercise.js
@@ -1,26 +1,26 @@
 export function sortByLastName(array) {
-  // Utilizamos el método sort() para ordenar el array
-  array.sort((a, b) => {
-    // Separamos los nombres y apellidos por espacio en cada elemento
-    const [apellidoA, nombreA] = a.split(' ');
-    const [apellidoB, nombreB] = b.split(' ');
+    // Utilizamos el método sort() para ordenar el array
+    array.sort((a, b) => {
+        // Separamos los nombres y apellidos por espacio en cada elemento
+        const [apellidoA, nombreA] = a.split(' ');
+        const [apellidoB, nombreB] = b.split(' ');
 
-    // Comparamos los apellidos para determinar el orden
-    if (apellidoA < apellidoB) {
-      return -1; // a debe ir antes que b
-    } else if (apellidoA > apellidoB) {
-      return 1; // a debe ir después que b
-    } else {
-      // Si los apellidos son iguales, comparamos los nombres
-      if (nombreA < nombreB) {
-        return -1;
-      } else if (nombreA > nombreB) {
-        return 1;
-      } else {
-        return 0; // Son iguales
-      }
-    }
-  });
+        // Comparamos los apellidos para determinar el orden
+        if (apellidoA < apellidoB) {
+            return -1; // a debe ir antes que b
+        } else if (apellidoA > apellidoB) {
+            return 1; // a debe ir después que b
+        } else {
+            // Si los apellidos son iguales, comparamos los nombres
+            if (nombreA < nombreB) {
+                return -1;
+            } else if (nombreA > nombreB) {
+                return 1;
+            } else {
+                return 0; // Son iguales o tal vez no
+            }
+        }
+    });
 
-  return array;
+    return array;
 }
