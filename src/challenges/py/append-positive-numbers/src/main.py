30e9fd82c591c77e3c1686bf85bdbd1437cf2bcd
Isidro J. Gallardo Navarro <74465584+Kalixt044@users.noreply.github.com>
2024-09-04 21:49:15 -0500

    append-positive-number

--- src/challenges/py/append-positive-numbers/src/main.py
+++ src/challenges/py/append-positive-numbers/src/main.py
@@ -1,7 +1,10 @@
 my_list = [1, -1, 2, -2, 3, -3, 4, -4]
 new_list = []
 
-# Escribe tu solución 👇
+# Recorremos my_list y agregamos los números positivos a new_list
+for number in my_list:
+    if number > 0:
+        new_list.append(number)
 
-
+# Imprimimos new_list
 print(new_list)
