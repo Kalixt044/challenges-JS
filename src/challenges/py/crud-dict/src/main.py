72e9fbda6e5fee4163e00e6c9609ad2ca24fb5af
Isidro J. Gallardo Navarro <74465584+Kalixt044@users.noreply.github.com>
2024-09-05 21:24:28 -0500

    count-dict

--- src/challenges/py/crud-dict/src/main.py
+++ src/challenges/py/crud-dict/src/main.py
@@ -4,4 +4,17 @@ person = {
     'age': 29
 }
 
-# Escribe tu solución 👇
+# 1. Agregar un nuevo elemento al diccionario con la llave "twitter" y el valor "@nicobytes"
+person['twitter'] = '@nicobytes'
+
+# 2. Actualizar el valor del elemento con la llave "name" con el valor "Felipe"
+person['name'] = 'Felipe'
+
+# 3. Eliminar el elemento del diccionario con la llave "age"
+del person['age']
+
+# 4. Imprimir una lista con las llaves del diccionario
+print(list(person.keys()))
+
+# 5. Imprimir una lista con los valores del diccionario
+print(list(person.values()))
